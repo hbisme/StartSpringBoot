8e9d38c56a233cf6ca9d02822234b492a4f1144b
hbisme <343122422@qq.com>
2018-09-16 22:58:52 +0800

    日志样例学习

---
+++ src/main/java/com/hb/start_boot/controller/TestController.java
@@ -0,0 +1,31 @@
+package com.hb.start_boot.controller;
+
+
+/* 测试logback打印日志
+*
+* */
+
+import com.hb.start_boot.dao.User;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/log")
+public class TestController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestController.class);
+
+    @RequestMapping("/user")
+    public User getUser(){
+        User user = new User(1L, "hb", "888");
+        LOGGER.info("执行展示用户函数");
+        LOGGER.error("测试错误日志输出, user: {}", user);
+
+        return user;
+    }
+
+
+
+}
